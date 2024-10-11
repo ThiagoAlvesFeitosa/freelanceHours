74862d696e2ffd71699305a38eea19e236a9cbd1
ThiagoAlvesFeitosa <thiago.feitosa.alves@hotmail.com>
2024-10-11 18:17:46 -0300

    implementacao de fila de emails junto com a finalizacao das propostas

---
+++ resources/views/components/ui/icons/vue.blade.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 261.76 226.69" class="icon-vue">
+  <g fill="none">
+    <polygon fill="#42B883" points="130.89 0 0 0 130.88 226.69 261.76 0 130.89 0"/>
+    <polygon fill="#35495E" points="43.71 0 130.88 141.43 218.06 0 174.35 0 130.88 69.79 87.42 0 43.71 0"/>
+  </g>
+</svg>
